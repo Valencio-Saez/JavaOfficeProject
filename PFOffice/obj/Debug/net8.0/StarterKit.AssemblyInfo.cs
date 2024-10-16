c22ccad61d09b5c4e04834ef9d727f06d781ff8d
Jay1317 <125268292+Jay1317@users.noreply.github.com>
2024-10-16 12:57:39 +0200

    test

--- PFOffice/obj/Debug/net8.0/StarterKit.AssemblyInfo.cs
+++ PFOffice/obj/Debug/net8.0/StarterKit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StarterKit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1e6e6b0c09eb94624dfa9478ba686f306cad5e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2e86cc42d6b0c962d10a06d65f0aa3771754186")]
 [assembly: System.Reflection.AssemblyProductAttribute("StarterKit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StarterKit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
