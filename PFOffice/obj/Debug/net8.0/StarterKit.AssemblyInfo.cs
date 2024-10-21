5fdfa532012928a0deb4a9011f9ce3c2a7a53100
babatsehaie124 <125268407+babatsehaie124@users.noreply.github.com>
2024-10-22 00:29:23 +0200

    ATTENDANCE FIX

--- PFOffice/obj/Debug/net8.0/StarterKit.AssemblyInfo.cs
+++ PFOffice/obj/Debug/net8.0/StarterKit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StarterKit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44a6820d9d74e4721597907254ef89f472e2718c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+428fd26cc54af02c373d44da3824bd31f2ef2d5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("StarterKit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StarterKit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
