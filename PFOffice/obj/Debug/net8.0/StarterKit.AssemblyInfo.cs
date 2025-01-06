44a8d3b1f5da92bd2fe79ea20e4b5381ca605eef
Jay1317 <125268292+Jay1317@users.noreply.github.com>
2025-01-06 22:30:00 +0100

    changes

--- PFOffice/obj/Debug/net8.0/StarterKit.AssemblyInfo.cs
+++ PFOffice/obj/Debug/net8.0/StarterKit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StarterKit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cee39573c27bbf693eda604ed9eab9cab04c1fb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8481c21940f694e2fe0b7cfffed284f7d0f232d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("StarterKit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StarterKit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ PFOffice/wwwroot/js/app.01271bdd9adb95379923.hot-update.js
@@ -0,0 +1,29 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdatehello_react_typescript"]("app",{
+
+/***/ "./src/pages/Home.tsx":
+/*!****************************!*\
+  !*** ./src/pages/Home.tsx ***!
+  \****************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module parse failed: Identifier 'navigate' has already been declared (46:10)\\nFile was processed with these loaders:\\n * ./node_modules/ts-loader/index.js\\nYou may need an additional loader to handle the result of these loaders.\\n|         navigate(`/events/${id}`); // Navigeren naar de detailpagina van een evenement\\n|     };\\n>     const navigate = (0, react_router_dom_1.useNavigate)();\\n|     const goToAdminDashboard = () => {\\n|         navigate('/admin');\");\n\n//# sourceURL=webpack://hello-react-typescript/./src/pages/Home.tsx?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("ed48dc2888613ea41551")
+/******/ })();
+/******/ 
+/******/ }
+);
