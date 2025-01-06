bc455e42993569172f2f5211aec7d131fd305701
AiCon3R <1054255@hr.nl>
2025-01-06 14:32:04 +0100

    Start

--- PFOffice/Program.cs
+++ PFOffice/Program.cs
@@ -1,11 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using StarterKit.Models;
 using StarterKit.Services;
-
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
-
 namespace StarterKit
 {
     class Program
@@ -13,30 +11,19 @@ namespace StarterKit
         static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddControllersWithViews()
-                .AddJsonOptions(options =>
-                {
-                    // Hiermee wordt objectcyclus gedetecteerd en opgelost
-                    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
-                    options.JsonSerializerOptions.WriteIndented = true; // Optioneel: voor beter leesbare JSON output
-                });
 
             builder.Services.AddControllersWithViews();
             builder.Services.AddDistributedMemoryCache();
             builder.Services.AddSession(options =>
             {
-                // options.IdleTimeout = TimeSpan.FromSeconds(10);
-                options.IdleTimeout = TimeSpan.FromMinutes(120);
+                options.IdleTimeout = TimeSpan.FromSeconds(10);
                 options.Cookie.HttpOnly = true;
                 options.Cookie.IsEssential = true;
             });
-            builder.Services.AddScoped<IAttendanceService, AttendanceService>();
             builder.Services.AddScoped<ILoginService, LoginService>();
             builder.Services.AddScoped<IEventService, EventService>();
             builder.Services.AddDbContext<DatabaseContext>(
                 options => options.UseSqlite(builder.Configuration.GetConnectionString("SqlLiteDb")));
-
-
 
             // Authentication services
             builder.Services.AddAuthentication(options =>
@@ -44,7 +31,7 @@ namespace StarterKit
                 options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
                 options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
             })
-                .AddJwtBearer(options =>
+            .AddJwtBearer(options =>
             {
                 options.TokenValidationParameters = new TokenValidationParameters
                 {
@@ -57,11 +44,6 @@ namespace StarterKit
                     IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("YourSecretKey"))
                 };
             });
-
-
-
-
-
 
             var app = builder.Build();
 
@@ -77,18 +59,19 @@ namespace StarterKit
             app.UseStaticFiles();
             app.UseRouting();
 
-            app.UseSession();
-
             app.UseAuthentication();
             app.UseAuthorization();
 
+            app.UseSession();
 
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}"
                 );
 
-            app.MapGet("/", () => Results.Content("Calender 2 Application", "text/html"));
+            // Serve React app
+            app.MapFallbackToFile("index.html");
+
             app.Run();
         }
     }
