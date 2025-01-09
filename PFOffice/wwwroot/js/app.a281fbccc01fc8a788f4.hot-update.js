d22e9a26c72811e6e0e4c6634f2594657015be97
Valencio-Saex <valenciosaez.ict@gmail.com>
2025-01-09 06:26:38 +0100

    accesibility finish

---
+++ PFOffice/wwwroot/js/app.a281fbccc01fc8a788f4.hot-update.js
@@ -0,0 +1,29 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdatehello_react_typescript"]("app",{
+
+/***/ "./node_modules/css-loader/dist/cjs.js!../wwwroot/css/AccessibilityOptions.css":
+/*!*************************************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js!../wwwroot/css/AccessibilityOptions.css ***!
+  \*************************************************************************************/
+/***/ ((module, exports, __webpack_require__) => {
+
+eval("exports = module.exports = __webpack_require__(/*! ../../Frontend/node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\")(false);\n// Module\nexports.push([module.id, \".access ul {\\n    list-style-type: none;\\n  }\\n  \\n  #accessibility-button img {\\n    width: 50px;\\n    height: 50px;\\n  }\\n  \\n  .high-contrast {\\n    color: #f00;\\n    -webkit-filter: invert(100%);\\n    filter: invert(100%);\\n  }\\n  \\n  .grey-hues {\\n    filter: grayscale(100%);\\n  }\\n  \\n  .colorblind {\\n    width: 30%;\\n    border: none;\\n    background-color: transparent;\\n    display: flex;\\n    justify-content: center;\\n    align-items: center;\\n    cursor: pointer;\\n    margin: -30px;\\n  }\\n  \\n  .colorchange {\\n    width: 100%;\\n    height: 40%;\\n    align-items: center;\\n  }\\n  \\n  body.colorblind-mode {\\n    background-color: #DDCC77;\\n  }\\n  \\n  body.large-text {\\n    font-size: 20px;\\n  }\\n\\n  .black-white-mode {\\n    background-color: #000000; /* Zwarte achtergrond */\\n    color: #ffffff; /* Witte tekst */\\n  }\\n  \\n  .black-white-mode a {\\n    color: #00ffff; /* Optionele kleur voor links in wit */\\n  }\\n  \\n  .black-white-mode button {\\n    background-color: #222222; /* Donkergrijs voor knoppen */\\n    color: #ffffff;\\n    border: 1px solid #ffffff; /* Witte rand voor contrast */\\n  }\\n  \\n  .black-white-mode h1, \\n  .black-white-mode h2, \\n  .black-white-mode h3 {\\n    color: #ffffff; /* Witte kleur voor koppen */\\n  }\", \"\"]);\n\n\n\n//# sourceURL=webpack://hello-react-typescript/../wwwroot/css/AccessibilityOptions.css?./node_modules/css-loader/dist/cjs.js");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("70c26772a55f0e3dd72b")
+/******/ })();
+/******/ 
+/******/ }
+);
